a500180260fdee125e60e71695194253393db717
KimHao Nguyen <ndkimhao@users.noreply.github.com>
2023-02-05 03:00:36 -0600

    block ram

--- fpga/fpga.srcs/sources_1/new/RAM.vhd
+++ fpga/fpga.srcs/sources_1/new/RAM.vhd
@@ -18,46 +18,56 @@ end RAM;
 
 architecture Behavioral of RAM is
 
-type TArrRam is array (RAMSize/2-1 downto 0) of TByte;
-
-signal ram_a : TArrRam := (others => (others => '0'));
-signal ram_b : TArrRam := (others => (others => '0'));
+component blk_mem_gen_0
+	port (
+		clka : in std_logic;
+		ena : in std_logic;
+		wea : in std_logic_vector(0 downto 0);
+		addra : in std_logic_vector(16 downto 0);
+		dina : in std_logic_vector(7 downto 0);
+		douta : out std_logic_vector(7 downto 0)
+	);
+end component;
 
 signal addr2 : unsigned(PhyAddrWidth-2 downto 0);
 signal addr2plus1 : unsigned(PhyAddrWidth-2 downto 0);
+signal aligned : std_logic;
+
+signal din_a, dout_a : TByte;
+signal din_b, dout_b : TByte;
+signal addr_a, addr_b : unsigned(16 downto 0);
 
 begin
 
 	addr2 <= unsigned(addr(PhyAddrWidth-1 downto 1));
 	addr2plus1 <= addr2 + 1;
+	aligned <= not addr(0);
 
-	process(clk)
-	begin
-	
-		if rising_edge(clk) then
-			if en = '1' then
-				if addr(0) = '0' then
-					if wr = '1' then
-						ram_b(to_integer(addr2)) <= din(15 downto 8);
-						ram_a(to_integer(addr2)) <= din(7 downto 0);
-						dout <= din;
-					else
-						dout <= ram_b(to_integer(addr2)) & ram_a(to_integer(addr2));
-					end if;
-				else
-					if wr = '1' then
-						ram_a(to_integer(addr2)+1) <= din(15 downto 8);
-						ram_b(to_integer(addr2)) <= din(7 downto 0);
-						dout <= din;
-					else
-						dout <= ram_a(to_integer(addr2plus1)) & ram_b(to_integer(addr2));
-					end if;
-				end if;
-			else
-				dout <= (others => '0');
-			end if;
-		end if;
-	
-	end process;
+	din_a <= din(7 downto 0)  when aligned = '1' else din(15 downto 8);
+	din_b <= din(15 downto 8) when aligned = '1' else din(7 downto 0);
+
+	addr_a <= addr2(16 downto 0) when aligned = '1' else addr2plus1(16 downto 0);
+	addr_b <= addr2(16 downto 0);
+
+	dout <= dout_b & dout_a when aligned = '1' else
+			dout_a & dout_b;
+
+	blk_a : blk_mem_gen_0 PORT MAP (
+		clka => clk,
+		ena => en,
+		wea => wr & "",
+		addra => std_logic_vector(addr_a),
+		dina => din_a,
+		douta => dout_a
+	);
+
+	blk_b : blk_mem_gen_0 PORT MAP (
+		clka => clk,
+		ena => en,
+		wea => wr & "",
+		addra => std_logic_vector(addr_b),
+		dina => din_b,
+		douta => dout_b
+	);
 
 end Behavioral;
