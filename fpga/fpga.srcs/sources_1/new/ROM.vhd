5748ba53a2a28be7c1419323921bc4e01c4de24a
KimHao Nguyen <ndkimhao@users.noreply.github.com>
2023-02-05 02:05:22 -0600

    upd

--- fpga/fpga.srcs/sources_1/new/ROM.vhd
+++ fpga/fpga.srcs/sources_1/new/ROM.vhd
@@ -16,6 +16,10 @@ end ROM;
 
 architecture Behavioral of ROM is
 
+signal addr0 : unsigned(PhyAddrWidth-1 downto 0);
+signal addr1 : unsigned(PhyAddrWidth-1 downto 0);
+signal addr_valid : std_logic;
+
 constant ROMSize : integer := 6;
 type TArrROM is array(0 to ROMSize) of TByte;
 signal data_rom : TArrROM := (
@@ -26,13 +30,17 @@ signal data_rom : TArrROM := (
 
 begin
 
+	addr0 <= unsigned(addr);
+	addr1 <= unsigned(addr) + 1;
+	addr_valid <= '1' when addr1 < ROMSize else '0';
+
 	process(clk)
 	begin
 	
 		if rising_edge(clk) then
-		
-			if unsigned(addr) < ROMSize - 1 then
-				dout <= data_rom(to_integer(unsigned(addr)) + 1) & data_rom(to_integer(unsigned(addr)));
+
+			if addr_valid = '1' then
+				dout <= data_rom(to_integer(addr1)) & data_rom(to_integer(addr0));
 				valid <= '1';
 			else
 				dout <= (others => '0');
