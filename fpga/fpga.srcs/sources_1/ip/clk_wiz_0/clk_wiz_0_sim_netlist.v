109da933de10cf5f9ab5483a0da941712800e3f1
KimHao Nguyen <ndkimhao@users.noreply.github.com>
2023-02-05 23:33:39 -0600

    ckk 70mhz

--- fpga/fpga.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_sim_netlist.v
+++ fpga/fpga.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_sim_netlist.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-// Date        : Sun Feb  5 18:22:24 2023
+// Date        : Sun Feb  5 23:32:43 2023
 // Host        : tpe running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode funcsim
 //               d:/School/weeekly_processor/fpga/fpga.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_sim_netlist.v
@@ -92,12 +92,12 @@ module clk_wiz_0_clk_wiz_0_clk_wiz
   (* BOX_TYPE = "PRIMITIVE" *) 
   MMCME2_ADV #(
     .BANDWIDTH("OPTIMIZED"),
-    .CLKFBOUT_MULT_F(10.000000),
+    .CLKFBOUT_MULT_F(49.875000),
     .CLKFBOUT_PHASE(0.000000),
     .CLKFBOUT_USE_FINE_PS("FALSE"),
     .CLKIN1_PERIOD(10.000000),
     .CLKIN2_PERIOD(0.000000),
-    .CLKOUT0_DIVIDE_F(10.000000),
+    .CLKOUT0_DIVIDE_F(14.250000),
     .CLKOUT0_DUTY_CYCLE(0.500000),
     .CLKOUT0_PHASE(0.000000),
     .CLKOUT0_USE_FINE_PS("FALSE"),
@@ -127,7 +127,7 @@ module clk_wiz_0_clk_wiz_0_clk_wiz
     .CLKOUT6_PHASE(0.000000),
     .CLKOUT6_USE_FINE_PS("FALSE"),
     .COMPENSATION("ZHOLD"),
-    .DIVCLK_DIVIDE(1),
+    .DIVCLK_DIVIDE(5),
     .IS_CLKINSEL_INVERTED(1'b0),
     .IS_PSEN_INVERTED(1'b0),
     .IS_PSINCDEC_INVERTED(1'b0),
